ee1159a63b533295ee8d8a2b0319b5556ff051d0
Becky Rosa <becky_m_rosa@yahoo.com>
2020-12-30 23:16:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,12 +9,12 @@ if (board[index] == "X") || (board[index] == "O")
   true
 elsif board[index] == " " || "" || nil
   false
-
   end
 end
 
-def valid_move?(board, position)
-  index = position.to_i-1
+
+def valid_move?(board, index)
+  index.to_i-1
   if index.between?(0,8) && !position_taken?(board, index)
     true
   else
