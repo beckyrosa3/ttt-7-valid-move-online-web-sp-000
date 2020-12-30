5b71693dcff7bbaef34325193d8e8edb67d06479
Becky Rosa <becky_m_rosa@yahoo.com>
2020-12-30 23:03:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,5 +15,5 @@ end
 
 def valid_move?(board, position)
   index = position.to_i-1
-  (board[position].between?(0,8)) && (!position_taken)
+  index.between?(0,8) && !position_taken?(board, index)
 end
